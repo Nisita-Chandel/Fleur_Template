10a60d1b286a6fce8c7662d84249262b4c2bcf40
Nisita Chandel <nisitachandel@gmail.com>
2025-08-31 22:20:47 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Nav/Navbar'
+import Page1 from './Page1/Page1'
+
+const App = () => {
+  return (
+    <div className='mt-5'>
+
+      <Navbar/>
+      <Page1/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Nav/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-[#EFE2D9] flex gap-10 justify-between items-center'>
+        <img className='h-20 ml-20' src="https://sproviero-type.com/_astro/fleur.B9lZftXp_19naF5.svg" alt="" />
+      <div className='flex gap-30 mr-30 text-sm font-[500] '>
+       <h1 className='hover:bg-[#E6CDB9] px-4 py-2'>COLLECTION</h1>
+       <h1 className='hover:bg-[#E6CDB9] px-4 py-2'>ABOUT</h1>
+       <h1 className='hover:bg-[#E6CDB9] px-4 py-2 '>CONTACT</h1>
+       <h1 className='hover:bg-[#E6CDB9] px-4 py-2'>SHOP</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
