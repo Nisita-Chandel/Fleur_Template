1ea50eee75f809a5173be77db523073bfd315293
Nisita Chandel <nisitachandel@gmail.com>
2025-09-03 00:20:51 +0530

    add new page

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -4,7 +4,7 @@ const Page2 = () => {
 
   return (
     <div className='bg-[#EFE2D9] text-center p-10'>
-      <h1 className='text-[#D5B8A1] text-xl font-[300]'>THIS IS WHAT WE DO</h1>
+      <h1 className='text-[#D5B8A1] text-xl font-[500]'>THIS IS WHAT WE DO</h1>
       <h1 className='text-black text-4xl font-[400] mt-4'>OUR SERVICES</h1>
       <div>
 
--- src/Page3.jsx
+++ src/Page3.jsx
@@ -25,15 +25,21 @@ const Page3 = () => {
 
 
   return (
-    //  <div className='bg-[#EFE2D9] '>
-     <div className='bg-[#EFE2D9] '>
-        <div className='bg-black h-80 w-90 ml-30 rounded-t-full mb-30 overflow-hidden' >
-                <img className='' src="https://i.pinimg.com/1200x/1f/8f/cb/1f8fcb15a6b60b7701c9324ff2886dfd.jpg" alt="" />
+     <div className='bg-[#EFE2D9] flex flex-row '>
+        { arr.map((elem,idx) =>(
 
-            <h1>custom </h1>
-            <h1>text</h1>
-            <button>read more</button>
+
+       <div className='bg-black h-100 w-90 ml-30 rounded-t-full mb-20 overflow-hidden ' key ={idx} >
+            <img className='' src= {elem.img} alt="" />
+         
+           <div className='absolute top-300 bg-[#E6CDB9] w-90 h-41 text-center ' >
+             <h1 className='mt-5 text-2xl'>{elem.heading}</h1>
+             <h1 className='text-sm mt-2'>{elem.text}</h1>
+             <button className='text-[15px] mt-2'>{elem.btn}</button>
+            </div>
         </div>
+  ))}
+
     
      </div> 
   )
@@ -44,15 +50,68 @@ export default Page3
 
 
 
-{/* <div className='bg-red-300 w-full h-full'>
-{arr.map((elem,idx) =>{
-    <div className='ml-30 flex gap-3' key= {idx}>
-        <img className='w-90 h-20 object-cover ' src = {elem.img} alt="" />
-        <h1 className='absolute ml-14 mt-5 text-[#605f5f] text-2xl text-center w-50'>{elem.heading}</h1>
-        <p className='absolute ml-15 top-430 text-gray-500 text-black text-sm'>{elem.text}</p>
-        <button className='absolute ml-15 top-430 text-gray-500 text-black text-sm'>{elem.btn}</button>
-    </div>
-})}
-</div>  */}
 
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+
+// const Page3 = () => {
+//   const arr = [
+//     {
+//       img: "https://i.pinimg.com/1200x/66/c2/5a/66c25a34ef6f756dbdc51aa92f1d02a5.jpg",
+//       heading: "Custom Bouquets",
+//       text: "Favourite flower for your special day or the perfect decoration for your next business event",
+//       btn: "READ MORE"
+//     },
+//     {
+//       img: "https://i.pinimg.com/1200x/1f/8f/cb/1f8fcb15a6b60b7701c9324ff2886dfd.jpg",
+//       heading: "Occasion Flowers",
+//       text: "Favourite flower for your special day or the perfect decoration for your next business event",
+//       btn: "READ MORE"
+//     },
+//     {
+//       img: "https://i.pinimg.com/1200x/7a/b1/88/7ab1882c5192dbbc73324043fc1335cb.jpg",
+//       heading: "Premade Bouquets",
+//       text: "Favourite flower for your special day or the perfect decoration for your next business event",
+//       btn: "READ MORE"
+//     },
+//   ];
+
+//   return (
+//     <div className="bg-[#EFE2D9] p-5 flex flex-row gap-30 items-center">
+//       {arr.map((elem, idx) => (
+//         <div
+//           key={idx}
+//           className="relative bg-black h-[400px] w-[30%]  rounded-t-full mb-5 overflow-hidden"
+//         >
+//           {/* Image */}
+//           <img
+//             className="w-full h-full object-cover"
+//             src={elem.img}
+//             // alt={elem.heading}
+//           />
+
+//           {/* Overlay Content */}
+//           <div className="absolute bottom-0 bg-[#E6CDB9] w-full h-[150px]  text-center p-2">
+//             <h1 className="text-xl mt-5 font-bold">{elem.heading}</h1>
+//             <p className="text-sm">{elem.text}</p>
+//             <button className="mt-1 text-base font-semibold hover:underline">
+//               {elem.btn}
+//             </button>
+//           </div>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default Page3;
 
